c6dff6020bfc7263f70f7158c60630ef5ece8d51
GDuath <duath83@gmx.de>
2025-05-20 00:40:45 +0200

    Siege bugfix, small adjustments

--- src/main/java/org/unitedlands/classes/WarGoal.java
+++ src/main/java/org/unitedlands/classes/WarGoal.java
@@ -1,7 +1,19 @@
 package org.unitedlands.classes;
 
 public enum WarGoal {
-    DEFAULT,
-    SKIRMISH,
-    INDEPENDENCE    
+    DEFAULT("Superiority"),
+    SKIRMISH("Skirmish"),
+    PLUNDER("Plunder"),
+    INDEPENDENCE("War for Independence");
+
+    private final String displayName;
+
+    private WarGoal(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
